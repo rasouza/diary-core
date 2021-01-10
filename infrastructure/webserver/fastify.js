2d15b00f6977438ae4d5d5d9422b359454c627b0
R. A. Souza <alves.wm@gmail.com>
2021-01-10 14:20:35 -0300

    disable sentry

--- infrastructure/webserver/fastify.js
+++ infrastructure/webserver/fastify.js
@@ -2,7 +2,6 @@ const each = require('lodash/fp/each')
 const fastify = require('fastify')
 const fastifyStatic = require('fastify-static')
 const helmet = require('fastify-helmet')
-const sentry = require('fastify-sentry')
 const path = require('path')
 
 const setupSchemas = server => each(schema => server.addSchema(schema))
@@ -23,10 +22,6 @@ module.exports = ({ routes, routeSchemas, FastifyLogger }) => {
   })
 
   server.register(helmet)
-  server.register(sentry, {
-    dsn: process.env.SENTRY_DSN,
-    environment: process.env.NODE_ENV
-  })
 
   setupSchemas(server)(routeSchemas)
   setupRoutes(server)(routes)
