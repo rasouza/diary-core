02bfeb43148c648e361f420d2619c828e88b4413
R. A. Souza <alves.wm@gmail.com>
2021-01-10 15:36:06 -0300

    writes stories integration test

--- infrastructure/webserver/errors.js
+++ infrastructure/webserver/errors.js
@@ -9,30 +9,6 @@ function NotFoundError (description) {
 NotFoundError.prototype = Object.create(Error.prototype)
 NotFoundError.prototype.constructor = NotFoundError
 
-function AlreadyExistsError (description) {
-  Error.call(this)
-  Error.captureStackTrace(this)
-  this.name = 'ALREADY_EXISTS'
-  this.statusCode = 409
-  this.message = description
-  this.isOperational = true
-}
-AlreadyExistsError.prototype = Object.create(Error.prototype)
-AlreadyExistsError.prototype.constructor = AlreadyExistsError
-
-function InvalidData (description) {
-  Error.call(this)
-  Error.captureStackTrace(this)
-  this.name = 'INVALID_DATA'
-  this.statusCode = 422
-  this.message = description
-  this.isOperational = true
-}
-InvalidData.prototype = Object.create(Error.prototype)
-InvalidData.prototype.constructor = InvalidData
-
 module.exports = {
-  NotFoundError,
-  AlreadyExistsError,
-  InvalidData
+  NotFoundError
 }
