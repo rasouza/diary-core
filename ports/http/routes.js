77acadf552e91dc8eafe99ff98abc7b977c684e5
R. A. Souza <alves.wm@gmail.com>
2021-01-10 14:19:41 -0300

    implements output serialization

--- ports/http/routes.js
+++ ports/http/routes.js
@@ -3,7 +3,10 @@ module.exports = ({ StoriesController }) => [
     method: 'POST',
     path: '/stories',
     schema: {
-      body: { $ref: 'StorySchema#' }
+      body: { $ref: 'StorySchema#/definitions/body' },
+      response: {
+        201: { $ref: 'StorySchema#/definitions/response/2xx' }
+      }
     },
     handler: StoriesController.create
   }
