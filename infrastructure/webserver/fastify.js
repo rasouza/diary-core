b7a583aba10542f196089685ddc2e08e55656b79
R. A. Souza <alves.wm@gmail.com>
2021-01-10 03:44:26 -0300

    encapsulates fastify bootstrap

--- infrastructure/webserver/fastify.js
+++ infrastructure/webserver/fastify.js
@@ -1,36 +1,31 @@
 const each = require('lodash/fp/each')
 const fastify = require('fastify')
 const fastifyStatic = require('fastify-static')
+const helmet = require('fastify-helmet')
 const path = require('path')
 
-module.exports = ({ routes, routeSchemas, FastifyLogger }) => {
-  const server = fastify({
-    logger: FastifyLogger
-  })
+const setupSchemas = server => each(schema => server.addSchema(schema))
+const setupRoutes = server => each(path => server.route(path))
 
-  /**
-   * Setup route schemas
-   */
-  each(schema => {
-    server.addSchema(schema)
-  })(routeSchemas)
-
-  /**
-   * Setup routes
-   */
-  each(path => {
-    server.route(path)
-  })(routes)
-
-  /**
-   * Setup Docs and Coverage static file serving
-   */
+const setupStatic = server => {
   server.register(fastifyStatic, {
     root: path.join(__dirname, '../..'),
     redirect: true
   })
   server.get('/docs/', (req, res) => { res.sendFile('docs/') })
   server.get('/coverage/', (req, res) => { res.sendFile('coverage/') })
+}
+
+module.exports = ({ routes, routeSchemas, FastifyLogger }) => {
+  const server = fastify({
+    logger: FastifyLogger
+  })
+
+  server.register(helmet)
+
+  setupSchemas(server)(routeSchemas)
+  setupRoutes(server)(routes)
+  setupStatic(server)
 
   return server
 }
