d46eda91f74a63e4b76e44a46f349a04c2af0b6d
R. A. Souza <alves.wm@gmail.com>
2021-04-25 13:17:09 -0300

    fix(runtime): listens to all network interfaces

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('make-promises-safe')
 const container = require('./infrastructure/config/container')()
 const server = require('./infrastructure/webserver/fastify')(container.cradle)
 
-server.listen(3001, (err, address) => {
+server.listen(3001, '0.0.0.0', (err, address) => {
   if (err) {
     server.log.error(err)
     process.exit(1)
