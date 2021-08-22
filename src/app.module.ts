f4495d168f9a3523d8450d132286c4268d459ba9
R. A. Souza <alves.wm@gmail.com>
2021-08-22 02:43:25 -0300

    feat(api): improve API security
    implements CORS, Helmet and CookieParser

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,17 @@ import { AppService } from './app.service';
 import { StoriesModule } from './stories/stories.module';
 import { ConfigModule } from '@nestjs/config';
 import { SupabaseModule } from './supabase/supabase.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     StoriesModule,
     ConfigModule.forRoot({ isGlobal: true }),
     SupabaseModule,
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
@@ -7,6 +9,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  app.enableCors();
+  app.use(cookieParser());
+  app.use(helmet());
 
   const config = new DocumentBuilder()
     .setTitle('Programming Diary API')
