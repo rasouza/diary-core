1e4d3a887904b0b53a774658fa4f5f77a8b35420
R. A. Souza <alves.wm@gmail.com>
2021-09-02 17:42:14 -0300

    feat(errors): add sentry monitoring

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,21 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as Sentry from '@sentry/node';
+import { LogLevel } from '@sentry/types';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  Sentry.init({
+    dsn: configService.get('SENTRY_DSN'),
+    environment: configService.get('NODE_ENV'),
+    release: configService.get('npm_package_version'),
+    logLevel: LogLevel.Debug,
+    debug: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
